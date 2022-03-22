1fc014a5521eee0dc71ea3876a85baac7b301648
Mami Nishiwaki <mamypoco@gmail.com>
2022-03-21 18:37:58 -0700

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button, Card, CardImg, CardText, Col, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, FormGroup, Label, Modal,ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom'; 
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -150,31 +151,51 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
-        if (props.campsite) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <Breadcrumb>
-                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                            </Breadcrumb>
-                            <h2>{props.campsite.name}</h2>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="row" >
-                        <RenderCampsite campsite={props.campsite} />
-                        <RenderComments 
-                            comments={props.comments}
-                            addComment={props.addComment}
-                            campsiteId={props.campsite.id}
-                        />
+    if (props.isLoading) {
+        return (
+             <div className="container">
+                 <div classname="row">
+                     <Loading />
+                 </div>
+             </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
                     </div>
                 </div>
-            );
-        }
-        return <div />;
+            </div>
+        );
+    }
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row" >
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
+                </div>
+            </div>
+        );
+    }
+    return <div />;
     }
 
 export default CampsiteInfo;
